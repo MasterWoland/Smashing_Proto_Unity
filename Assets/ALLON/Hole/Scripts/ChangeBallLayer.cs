7f7ccff572b0808933e52b9dbec261fff9605be9
Mark <mark@allon.nl>
2020-06-25 15:17:32 +0200

    First commit

---
+++ Assets/ALLON/Hole/Scripts/ChangeBallLayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeBallLayer : MonoBehaviour {
+
+    public int LayerOnEnter; // BallInHole
+    public int LayerOnExit;  // BallOnTable
+    private readonly string _ballTag = "Ball";
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag(_ballTag))
+        {
+            Debug.Log("___ change layer! ____");
+            other.gameObject.layer = LayerOnEnter;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // if(other.gameObject.CompareTag(_ballTag))
+        // {
+        //     Debug.Log("___ reset layer ____");
+        //     other.gameObject.layer = LayerOnExit;
+        // }
+    }
+}
